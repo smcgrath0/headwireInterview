108b6e9af41dcda8d836a7aadb80fb8335fe5285
smcgrath0 <smcgrath7777@gmail.com>
2019-12-22 14:22:15 -0800

    added navbar color change pages dropdown arrow, imperfect solution

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
 export default class Navbar extends React.Component {
+  constructor(props){
+    super(props);
+
+  }
+  componentDidMount(){
+    window.onscroll = function (event) {
+      // console.log(event.srcElement.scrollingElement.scrollTop);
+      var myNav = document.querySelector('.navbar');
+      var logo = document.querySelector('.logo');
+
+      "use strict";
+      if (event.srcElement.scrollingElement.scrollTop >= 200) {
+        logo.classList.add('logo2');
+        logo.classList.remove('logo1');
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+      }
+      else {
+        logo.classList.add('logo1')
+        logo.classList.remove('logo2');
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+      }
+    };
+  }
   render(){
     return (
-      <div className="navbar">
-        <div className="logo">
+      <div className="navbar" >
+        <div className="logo logo1">
 
         </div>
         <div className="links">
@@ -20,8 +45,13 @@ export default class Navbar extends React.Component {
           <div className="link">
             Pricing
           </div>
-          <div className="link">
+          <div className="link pagesLink">
             Pages
+            {//can easily fix, if have fontawesome PRO, gives icon with adaptive color change instead of an image
+            }
+            <div className="pagesDropdown" onClick={(event) => {
+              console.log(event.target)
+            }}></div>
           </div>
           <div className="link">
             Contact Us
