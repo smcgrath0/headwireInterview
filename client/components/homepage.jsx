abc2315d1ec0143674167f719cd0f1ef53bd82c4
smcgrath0 <smcgrath7777@gmail.com>
2019-12-26 12:36:01 -0800

    made homepage a functional component

--- client/components/homepage.jsx
+++ client/components/homepage.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 
-export default class Homepage extends React.Component {
-  render() {
-    return (
-      <div className="homebg">
-        <div className="homebgContainer">
-          <div className="line">
+export default function Homepage(props) {
+  return (
+    <div className="homebg">
+      <div className="homebgContainer">
+        <div className="line">
 
-          </div>
-          <div className="homebgTextContainer">
-            <h1>Professional Business Consulting</h1>
-            <p>Business provides reliable business planning, management, and advisory services backed by years of experience in the industry.</p>
-            <div className="homebgButtonContainer">
-              <button className="readMore">Read more</button>
-              <button className="contactUs">Contact us</button>
-            </div>
+        </div>
+        <div className="homebgTextContainer">
+          <h1>Professional Business Consulting</h1>
+          <p>Business provides reliable business planning, management, and advisory services backed by years of experience in the industry.</p>
+          <div className="homebgButtonContainer">
+            <button className="readMore">Read more</button>
+            <button className="contactUs">Contact us</button>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
